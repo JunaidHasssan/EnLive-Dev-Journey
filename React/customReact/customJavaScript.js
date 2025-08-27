f6a11d4ea8daae82f81e3b796c1a5b91994ab7b7
Junaid Hasssan <junaidhassan0860@gmail.com>
2025-08-27 22:28:24 +0500

    created custom react by self

---
+++ React/customReact/customJavaScript.js
@@ -0,0 +1,23 @@
+function customRender(element, container) {
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domElement)
+    
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target:'_blank'
+    },
+    children: 'Google here'
+}
+
+const mainContainer = document.getElementById('root')
+
+customRender(reactElement, mainContainer)
